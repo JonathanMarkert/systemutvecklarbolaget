37d37cb8c776c60c79e50017f2e9462a9e684770
Johanna Skinnari <jskinnari@gmail.com>
2021-09-02 21:29:30 +0200

    #11 make footer layout

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,11 +1,33 @@
-import React, { FC } from "react";
+import Box from "@material-ui/core/Box";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
 
-interface Props {}
+const useStyles = makeStyles((theme: Theme) => 
+  createStyles({
+    footer: {
+      backgroundColor: "#7c3f04",
+      padding: theme.spacing(6),
+    },
+    textColor: {
+      color: "#d4ccce",
+    }
+}));
 
-export const Footer: FC<Props> = (props: Props) => {
+export const Footer = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <p>FOOTER</p>
+    <div className={classes.footer}>
+      <Typography className={classes.textColor} variant="subtitle1" align="center" color="textSecondary" component="p">
+          <FacebookIcon />
+          <InstagramIcon />
+          <TwitterIcon />
+      </Typography>
+      <Box textAlign="center" className={classes.textColor}>
+          Systemutveklar Bolaget 	&copy; {new Date().getFullYear()}
+      </Box>
     </div>
   );
 };
