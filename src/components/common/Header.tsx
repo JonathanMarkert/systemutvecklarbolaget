b6f0b2bea1fe3704875894ce350562eaa930e7e1
Alpha-85 <ljungdahlt@gmail.com>
2021-09-03 08:16:56 +0200

    #9 Basic layout + links

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,14 +1,83 @@
-import React, {FC} from 'react'
+import { Badge, Hidden } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
 
 
-interface Props {
-    
-}
 
-export const Header:FC<Props> = (props: Props) => {
-    return (
-        <div>
-            <p>HEADER</p>
-        </div>
-    )
+const image: string =
+  "https://cdn.systembolaget.se/4a51ce/globalassets/logo.svg";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      display: "flex",
+      // opacity: "50%",
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    appBarStyle: {
+      position: "fixed",
+      // backgroundImage: `url(${headerImage})`,
+      backgroundColor: "#342404",
+    },
+    titleStyle: {
+      flexGrow: 1,
+      fontFamily: "cursive",
+      color: "#7c3f04",
+    },
+    iconStyle: {
+      fontSize: 40,
+      color: "#7c3f04",
+      "&:hover": {
+        color: "#f46f04",
+      },
+    },
+    mainImage: {},
+  })
+);
+
+export default function MenuAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.appBarStyle}>
+        <Toolbar>
+          <Link to="/">
+          <IconButton>
+            <img src={image} alt="" />
+          </IconButton>
+          </Link>
+          <div className={classes.grow}>
+            <Hidden only="xs">
+              <Typography variant="h6" className={classes.titleStyle}>
+                SystemUtvecklarBolaget
+              </Typography>
+            </Hidden>
+          </div>
+          <div>
+            <Link to="/cart">
+            <IconButton aria-label="cart">
+              <Badge badgeContent={0} color={"primary"}>
+                <ShoppingCartOutlinedIcon className={classes.iconStyle} />
+              </Badge>
+            </IconButton>
+            </Link>
+            <Link to="/admin">
+            <IconButton>
+              <AccountCircle className={classes.iconStyle} />
+            </IconButton>
+            </Link>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
