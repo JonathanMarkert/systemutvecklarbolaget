b345e7fc491de795ce247f12d9a825ba75bad646
Alpha-85 <ljungdahlt@gmail.com>
2021-09-04 21:24:58 +0200

    initial CartContext

--- src/Mockdata.tsx
+++ src/Mockdata.tsx
@@ -3,37 +3,60 @@ export interface Product {
 	url: string;
   name: string;
   brewery: string;
-	description: string; 
+	description: string;
+  price: number;
+  amount: number;
 }
 
-export const products: Product[] = [{
-	id: '1',
-	url: 'https://product-cdn.systembolaget.se/productimages/24410181/24410181_400.png',
-  name:'Xmas override',
-  brewery: 'NerdBrewing',
-	description: 'Most people believe that the Hazed and Infused defined by an ice house gives a stink finger to a bud light, but they need to remember how accidentally the Heineken for another booze wakes up. The razor blade beer recognizes the shot inside a mug. When you see a coors light, it means that a highly paid Corona panics. For example, a college-educated dude indicates that an unstable colt 45 wastedly is a big fan of the burly Hops Alligator Ale. Indeed, a booze tries to seduce the hypnotic mating ritual.',
-},{
-  id: '2',
-	url: 'https://product-cdn.systembolaget.se/productimages/24547950/24547950_400.png',
-  name:'Infix',
-  brewery: 'NerdBrewing',
-	description: 'A corona light around a Keystone light wakes up, and a coors light overwhelmingly plays pinochle with another dirt-encrusted Harpoon. Any change can dumbly plan an escape from the childlike Wolverine Beer the slurly mean-spirited Hefeweizen, but it takes a real blue moon to befriend the Mango Beer. Sometimes the eagerly radioactive satellite brewery laughs out loud, but a Left Hand Milk Stout living with a Pilsner always intoxicatedly cooks cheese grits for a wanker around a lager! Now and then, the Sierra Nevada Pale Ale of the Red Stripe borrows money from a Hoptoberfest',
-},{
-  id: '3',
-	url: 'https://product-cdn.systembolaget.se/productimages/24559455/24559455_400.png',
-  name:'Override',
-  brewery: 'NerdBrewing',
-	description: 'The line dancer dies, but the Long Trail Ale can be kind to the Lone Star living with a Coors. The Fraoch Heather Ale defined by a Jamaica Red Ale rejoices, and a Corona inside a micro brew prays; however, an underhandedly tipsy Sam Adams plans an escape from an Octoberfest a lager. An almost shabby Sierra Nevada, a discusting Dixie Beer, and a booze are what made America great! The hops around the satellite brewery has a change of heart about a Miller over a PBR, because a Sam Adams tries to seduce a corona light toward an Avery IPA. A Fosters often trades baseball cards with a Keystone about another black velvet.',
-},{
-  id: '4',
-	url: 'https://systembevakningsagenten.se/assets/images/product/id/8612.png',
-  name:'Discovery',
-  brewery: 'Reflection',
-	description: 'Furthermore, the mitochondrial Imperial Stout trembles, and a booze sanitizes a wanker. Furthermore, the sake bomb beyond a Left Hand Milk Stout sweeps the floor, and a molten steam engine recognizes an ESB for the Luna Sea ESB. Another Sierra Nevada is hypnotic. Now and then, the green beer carelessly sanitizes a bud dry from a lager.',
-},{
-  id: '5',
-	url: 'https://glasbanken.se/wp-content/uploads/Nerdbrewing_Extends_Carrotcake-1500x1500.png',
-  name:'Extends',
-  brewery: 'NerdBrewing',
-	description: 'A Fraoch Heather Ale related to the St. Pauli Girl pours freezing cold booze on the knowingly childlike ESB. A booze beyond the Fosters is sloshed. Most people believe that a bar stool inside a line dancer reaches an understanding with a wanker, but they need to remember how completely a Lone Star from the monkey bite procrastinates.',
-}];
+export const products: Product[] = [
+  {
+    id: "1",
+    url: "https://product-cdn.systembolaget.se/productimages/24410181/24410181_400.png",
+    name: "Xmas override",
+    brewery: "NerdBrewing",
+    description:
+      "Most people believe that the Hazed and Infused defined by an ice house gives a stink finger to a bud light, but they need to remember how accidentally the Heineken for another booze wakes up. The razor blade beer recognizes the shot inside a mug. When you see a coors light, it means that a highly paid Corona panics. For example, a college-educated dude indicates that an unstable colt 45 wastedly is a big fan of the burly Hops Alligator Ale. Indeed, a booze tries to seduce the hypnotic mating ritual.",
+    price: 10,
+    amount: 0,
+  },
+  {
+    id: "2",
+    url: "https://product-cdn.systembolaget.se/productimages/24547950/24547950_400.png",
+    name: "Infix",
+    brewery: "NerdBrewing",
+    description:
+      "A corona light around a Keystone light wakes up, and a coors light overwhelmingly plays pinochle with another dirt-encrusted Harpoon. Any change can dumbly plan an escape from the childlike Wolverine Beer the slurly mean-spirited Hefeweizen, but it takes a real blue moon to befriend the Mango Beer. Sometimes the eagerly radioactive satellite brewery laughs out loud, but a Left Hand Milk Stout living with a Pilsner always intoxicatedly cooks cheese grits for a wanker around a lager! Now and then, the Sierra Nevada Pale Ale of the Red Stripe borrows money from a Hoptoberfest",
+    price: 10,
+    amount: 0,
+  },
+  {
+    id: "3",
+    url: "https://product-cdn.systembolaget.se/productimages/24559455/24559455_400.png",
+    name: "Override",
+    brewery: "NerdBrewing",
+    description:
+      "The line dancer dies, but the Long Trail Ale can be kind to the Lone Star living with a Coors. The Fraoch Heather Ale defined by a Jamaica Red Ale rejoices, and a Corona inside a micro brew prays; however, an underhandedly tipsy Sam Adams plans an escape from an Octoberfest a lager. An almost shabby Sierra Nevada, a discusting Dixie Beer, and a booze are what made America great! The hops around the satellite brewery has a change of heart about a Miller over a PBR, because a Sam Adams tries to seduce a corona light toward an Avery IPA. A Fosters often trades baseball cards with a Keystone about another black velvet.",
+    price: 10,
+    amount: 0,
+  },
+  {
+    id: "4",
+    url: "https://systembevakningsagenten.se/assets/images/product/id/8612.png",
+    name: "Discovery",
+    brewery: "Reflection",
+    description:
+      "Furthermore, the mitochondrial Imperial Stout trembles, and a booze sanitizes a wanker. Furthermore, the sake bomb beyond a Left Hand Milk Stout sweeps the floor, and a molten steam engine recognizes an ESB for the Luna Sea ESB. Another Sierra Nevada is hypnotic. Now and then, the green beer carelessly sanitizes a bud dry from a lager.",
+    price: 10,
+    amount: 0,
+  },
+  {
+    id: "5",
+    url: "https://glasbanken.se/wp-content/uploads/Nerdbrewing_Extends_Carrotcake-1500x1500.png",
+    name: "Extends",
+    brewery: "NerdBrewing",
+    description:
+      "A Fraoch Heather Ale related to the St. Pauli Girl pours freezing cold booze on the knowingly childlike ESB. A booze beyond the Fosters is sloshed. Most people believe that a bar stool inside a line dancer reaches an understanding with a wanker, but they need to remember how completely a Lone Star from the monkey bite procrastinates.",
+        price: 10,
+  amount: 0,
+  },
+];
---
+++ src/components/context/ProductContext.tsx
@@ -0,0 +1,77 @@
+import { FC, createContext, useState } from "react";
+import { Product } from "../../Mockdata";
+
+interface CartContext {
+  cart: Product[];
+  handleAddToCart: (beerProduct: Product) => void;
+  handleRemoveFromCart: (beerProduct: Product) => void;
+  incrementNumber: (num: number) => void;
+  decrementNumber: (num: number) => void;
+}
+
+
+const CartProvider: FC = (props) => {
+    const [cart, setCart] = useState<Product[]>([]);
+    
+    const handleAddToCart = (beerProduct: Product) => {
+        setCart((previousState) => {
+            const isProductInCart = previousState.find(
+                (item) => item.id === beerProduct.id
+                );
+                if (isProductInCart) {
+                    return previousState.map((item) =>
+                    item.id === beerProduct.id
+                    ? { ...item, amount: item.amount + 1 }
+                    : item
+                    );
+                }
+                return [...previousState, {...beerProduct, amount: 1}]
+                // alternativt i varukorgen foreacha arrayn och ha en ny property som plussar på beroende på hur många id av samma det är. 
+            });
+        };
+        const handleRemoveFromCart = (beerProduct: Product) => {
+            // remove from cart
+        };
+        const incrementNumber = (num: number) => {
+            // alternativt flytta till cart component och ändra inparameter
+            // ta in en product[] och gå igenom.
+            // add number
+        };
+        const decrementNumber = (num: number) => {
+            // subtract number
+        };
+        
+        return (
+            <ProductContext.Provider
+            value={{
+                cart,
+                handleAddToCart,
+                handleRemoveFromCart,
+                incrementNumber,
+                decrementNumber,
+            }}
+            >
+      {props.children}
+    </ProductContext.Provider>
+  );
+};
+export default CartProvider;
+
+
+export const ProductContext = createContext<CartContext>({
+  cart: [],
+  handleAddToCart: () => {},
+  handleRemoveFromCart: () => {},
+  incrementNumber: () => {},
+  decrementNumber: () => {},
+});
+
+
+// consume:a en context i en component:
+//const {cart, handleAddToCart} = useContext(ContextValue);
+
+// sen använda i t.ex onclick på en button
+// onClick={() => handleAddtoCart(product:Product)}
+
+// const getTotalProducts = (cart: Product[]) =>
+//   cart.reduce((sum: number, item) => sum + item.amount, 0);
