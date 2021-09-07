f0dcf02fa59c973c9dfc2b6d8f4855f642d3f249
Alpha-85 <ljungdahlt@gmail.com>
2021-09-07 11:21:42 +0200

    Arrow is animated

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,29 +1,44 @@
 import { Box, Theme, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
-import ArrowDownwardRoundedIcon from "@material-ui/icons/ArrowDownwardRounded";
+import KeyboardArrowDownOutlinedIcon from "@material-ui/icons/KeyboardArrowDownOutlined";
 
 const useStyles = makeStyles((theme: Theme) => ({
   heroBackground: {
-    padding: theme.spacing(0),
-    width: "100%",
-  },
-  logoStyle: {
     display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: theme.palette.primary.main,
+    position: "relative",
+    height: "100%",
+    width: "100%",
+    color: "#fff",
   },
   heroContainer: {
-    //display: "flex",
-    // flexDirection: "column",
-    // alignItems: "center",
-    // justifyContent: "center",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
     position: "absolute",
     top: 0,
     bottom: 0,
     right: 0,
     left: 0,
   },
+  videoContainer: {
+    display: "flex",
+    flexDirection: "column", 
+    minWidth: "100%",
+    opacity: 1, // testa utan filmen
+  },
+  heroVideo: {
+    objectFit: "contain",
+  },
+  iconStyle: {
+    fontSize: 100,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   headLine: {
+    fontWeight: 500,
     letterSpacing: "2px",
     textAlign: "center",
     marginLeft: "auto",
@@ -36,15 +51,26 @@ const useStyles = makeStyles((theme: Theme) => ({
     marginLeft: "auto",
     marginRight: "auto",
   },
-  visualIndicator: {
-    textAlign: "center",
+  animatedItem: {
+    animation: `$myEffect 3000ms ${theme.transitions.easing.easeInOut}`,
+    animationIterationCount: "infinite",
   },
-  videoContainer: {
+  "@keyframes myEffect": {
+    "0%": {
+      transform: "translateY(0)",
+    },
+    "50%": {
+      transform: "translateY(-25%)",
+    },
+    "100%": {
+      transform: "translateY(0)",
+    },
+  },
+  visualIndicator: {
     display: "flex",
     flexDirection: "column",
-  },
-  heroVideo: {
-    objectFit: "contain",
+    justifyContent: "center",
+    alignItems: "center",
   },
 }));
 
@@ -61,21 +87,19 @@ const HeroSection = () => {
         </video>
       </Box>
       <Box className={classes.heroContainer}>
-        <Box className={classes.logoStyle}>
-          <img
-            style={{ width: "10%" }}
-            src="https://nerdbrewing.files.wordpress.com/2015/04/nerdbrewing_logo_black.png"
-            alt=""
-          />
-        </Box>
-        <Typography className={classes.headLine} variant="h1">Beer for Nerds</Typography>
-        <Typography className={classes.breadText} variant="body1">
-          Beer for nerds2 made from overuse of throw expections bla bla bla bla
-          bla
+        <Typography className={classes.headLine} variant="h1">
+          Beer for Developers
+          <Typography className={classes.breadText} variant="body1">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
+            provident accusamus rem neque ipsa esse aliquid numquam eligendi
+            libero corrupti.
+          </Typography>
         </Typography>
         <Box className={classes.visualIndicator}>
-          <Typography variant="h5">SCROLL DOWN</Typography>
-          <ArrowDownwardRoundedIcon />
+          <Typography variant="h6">SCROLL DOWN</Typography>
+          <KeyboardArrowDownOutlinedIcon
+            className={`${classes.iconStyle} ${classes.animatedItem} `}
+          />
         </Box>
       </Box>
     </Box>
