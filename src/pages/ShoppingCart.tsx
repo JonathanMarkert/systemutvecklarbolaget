090f5a5a042a781ca20908f49f9da2e3960cd1da
Tina Johansson <91chrjoh@skola.boras.se>
2021-09-07 11:16:18 +0200

    fixed ratio on layout, however cart slips still acting bonkers

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,32 +1,54 @@
-import React from "react";
+
+import { CSSProperties } from "@material-ui/styles";
 import  ProductCard from "../components/ProductCard";
-import { Product, products } from "../Mockdata";
-import { Container } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+import { products } from "../Mockdata";
+
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import { grey } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  background: {
+    backgroundColor: 'white',
+  },
   welcomeToShoppingCart: {
     backgroundColor: 'blue',
     textAlign: 'center',
     color: 'white',
-    width: '32rem',
-    margin: 'auto',
+    marginBottom: 0,
     padding: '0.5rem'
   },
   wrapper: {
     backgroundColor: 'purple',
-    width: '32rem',
-    margin: 'auto',
-    padding: '0 0 30px 0'
+    textAlign: 'center',
+    color: 'white',
+    marginBottom: 0,
+    padding: '0.5rem'
   },
   cartSlip: {
     height: '100%',
     backgroundColor: 'grey',
-    marginBottom: '1rem'
+    marginBottom: '1rem',
+    marginLeft: '1rem',
+    padding: '1rem'
   },
-  cardSlipMedia: {
+  cartSlipMedia: {
     width: '100%',
     height: 100
   }
@@ -34,43 +56,82 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 interface Props {
-  product: Product
+  
 }
 
 export default function ShoppingCart(){
   const classes = useStyles();
+  
   return (
-  <>
-    <Container>
-        <div className={classes.welcomeToShoppingCart}>
-          <h1>ShoppingCart</h1>
-          // Vill ta in ett värde från vagnen //
-            <h2>Your cart is empthy</h2>
-        </div>
-        <Container className={classes.wrapper}>
-          <div className={classes.cartSlip}>
-            This is a cardslip
-            // Grid? // Name
-            <div className={classes.cardSlipMedia}>
-              content
-            </div>
-          </div>
-          <div className={classes.cartSlip}>
-            This is a cardslip
-            // Grid? // Name
-            <div className={classes.cardSlipMedia}>
-              content
-            </div>
-          </div>
-          <div className={classes.cartSlip}>
-            This is a cardslip
-            // Grid? // Name
-            <div className={classes.cardSlipMedia}>
-              content
-            </div>
-          </div>
+    <React.Fragment>
+      <CssBaseline/>
+
+      <main className={classes.background}>
+        <Container className={classes.welcomeToShoppingCart} maxWidth="md">
+          <h1>Shopping cart</h1>
         </Container>
-    </Container>
-  </>
+          <Container className={classes.wrapper} maxWidth="md">
+            <h2>Wrapper</h2>
+              <Grid container spacing={2}>
+                <div className={classes.cartSlip}>
+                  <h3>cart slip</h3>
+                    <div className={classes.cartSlipMedia}>
+                      <h4>cart slip media</h4>
+                    </div>
+                </div>
+                <div className={classes.cartSlip}>
+                  <h3>cart slip</h3>
+                    <div className={classes.cartSlipMedia}>
+                      <h4>cart slip media</h4>
+                    </div>
+                </div>
+                <div className={classes.cartSlip}>
+                  <h3>cart slip</h3>
+                    <div className={classes.cartSlipMedia}>
+                      <h4>cart slip media</h4>
+                    </div>
+                </div>
+                <div className={classes.cartSlip}>
+                  <h3>cart slip</h3>
+                    <div className={classes.cartSlipMedia}>
+                      <h4>cart slip media</h4>
+                    </div>
+                </div>
+
+              </Grid>
+          </Container>
+      </main>
+    </React.Fragment>
   );
 };
+
+//<Container maxWidth="md">
+/*<div className={classes.welcomeToShoppingCart}>
+<h1>ShoppingCart</h1>
+// Vill ta in ett värde från vagnen //
+  <h2>Your cart is empthy</h2>
+</div>
+<Container className={classes.wrapper}>
+<div className={classes.cartSlip}>
+  This is a cardslip
+  // Grid? // Name
+  <div className={classes.cardSlipMedia}>
+    content
+  </div>
+</div>
+<div className={classes.cartSlip}>
+  This is a cardslip
+  // Grid? // Name
+  <div className={classes.cardSlipMedia}>
+    content
+  </div>
+</div>
+<div className={classes.cartSlip}>
+  This is a cardslip
+  // Grid? // Name
+  <div className={classes.cardSlipMedia}>
+    content
+  </div>
+</div>
+</Container>
+</Container>*/
