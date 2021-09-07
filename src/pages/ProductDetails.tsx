00094c975b7701a454a45177d6f6904d214cc770
Daniel <79dansun@skola.boras.se>
2021-09-07 13:28:50 +0200

    #34-step-3-coupling-complete
    Co-authored-by: Alpha-85 <Alpha-85@users.noreply.github.com>
    Co-authored-by: Jonathan Markert <JonathanMarkert@users.noreply.github.com>
    Co-authored-by: JohannaSkinnari <jskinnari@gmail.com>

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,78 +1,73 @@
 import {
-  Container,
-  createStyles,
-  makeStyles,
-  Theme,
-  Typography,
   Button,
-  CardMedia,
-  Grid,
-  Paper,
+  CardMedia, Container,
+  createStyles, Grid, makeStyles, Paper, Theme,
+  Typography
 } from "@material-ui/core";
-
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import { FC, useContext } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { ProductContext } from "../components/context/ProductContext";
+import { Product, products } from "../Mockdata";
 
-import { Product } from "../Mockdata";
-
-interface Props {
-  product: Product
-}
-
-// interface Props extends RouteComponentProps <{id: string}> {}
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    page: {
-      display: "flex",
-      flexWrap: "wrap",
-      padding: theme.spacing(4),
-    },
-    containerStyle: {
-      display: "flex",
-      justifyContent: "flex-start",
-      alignItems: "flex-end",
-      padding: theme.spacing(1),
-    },
-    image: {
-      height: 500,
-      width: 'auto',
-      objectFit: 'contain',
-    },
-    card: {
-      display:'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      minWidth: 200,
-    },
-    root: {
-      padding: theme.spacing(5),
-      flexGrow: 1,
-      display: "flex",
-    },
-    paper: {
-      padding: theme.spacing(1),
-      textAlign: "center",
-      color: theme.palette.text.secondary,
-    },
-  })
+createStyles({
+  page: {
+    display: "flex",
+    flexWrap: "wrap",
+    padding: theme.spacing(4),
+  },
+  containerStyle: {
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "flex-end",
+    padding: theme.spacing(1),
+  },
+  image: {
+    height: 500,
+    width: 'auto',
+    objectFit: 'contain',
+  },
+  card: {
+    display:'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minWidth: 200,
+  },
+  root: {
+    padding: theme.spacing(5),
+    flexGrow: 1,
+    display: "flex",
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+})
 );
 
-// const testData = products.find((p) => p.id === "4");
+interface DetailsProps {}
 
-const ProductDetails:FC<Props> = ({product}) => {
+interface DetailsProps extends RouteComponentProps <{productid: string}> {}
+
+const ProductDetails: FC<DetailsProps> = ({ match}:DetailsProps) => {
+  
   const { handleAddToCart } = useContext(ProductContext);
   const classes = useStyles();
+  const productId = match.params.productid;
+  const product = products.find((p) => p.id === productId) as Product;
+   
   return (
+   
     <Container className={classes.root}>
       <Grid container direction={"row"}justifyContent={"center"}alignItems={"center"}>
       <Grid container spacing={2} >
         <Grid item xs={12} sm={6}>
           <div className={classes.card}>
-           <CardMedia className={classes.image} 
-           component='img'
+           <CardMedia className={classes.image}   
+           component='img'         
            image={product?.urlDetails} 
            />
           </div>
@@ -83,7 +78,7 @@ const ProductDetails:FC<Props> = ({product}) => {
             <Typography variant="h5">{product?.brewery}</Typography>
             <Typography>{product?.description}</Typography>
             <Button size="large" color="secondary" onClick={() => handleAddToCart(product)}>
-            <AddShoppingCartIcon style={{ fontSize: 50 }} color="secondary" />
+            <AddShoppingCartIcon style={{ fontSize: 50 }} color="secondary"/>
               Buy now
             </Button>
           </Paper>
