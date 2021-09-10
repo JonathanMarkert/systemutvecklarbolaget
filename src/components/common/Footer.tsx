4ae042a066b6d823bbb666fcea6e2443f1623de3
Tina Johansson <91chrjoh@skola.boras.se>
2021-09-10 09:16:45 +0200

    added hoover over icons in footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -15,7 +15,11 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     icon: {
       margin: "0 1rem",
-      fontSize: "2rem"
+      fontSize: "2rem",
+      "&:hover": {
+        color: "white",
+        opacity: 1,
+      },
     }
 }));
 
