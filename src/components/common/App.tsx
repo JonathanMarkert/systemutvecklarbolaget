473d26a82ae1bf4b761fb05b30b4b89f9863ce5e
Alpha-85 <ljungdahlt@gmail.com>
2021-09-13 16:10:22 +0200

    #80 added scrolltotop component

--- src/components/common/App.tsx
+++ src/components/common/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
 import CartProvider from '../context/CartContext';
 import ProductProvider from '../context/ProductContext';
+import ScrollToTop from '../ScrollToTop';
 import Layout from './Layout';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <ProductProvider>
       <CartProvider>
       <Layout/>
