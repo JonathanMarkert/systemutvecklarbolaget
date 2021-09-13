2abf2e664d32ddb928c0e3a8e6c5777c37506b5b
Alpha-85 <ljungdahlt@gmail.com>
2021-09-13 14:06:18 +0200

    infoText if cart is empty

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -9,6 +9,7 @@ import SlipCard from "../components/SlipCard";
 import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
 import EuroSymbolOutlinedIcon from "@material-ui/icons/EuroSymbolOutlined";
 import classNames from "classnames";
+import SentimentVeryDissatisfiedIcon from "@material-ui/icons/SentimentVeryDissatisfied";
 
 const useStyles = makeStyles((theme) => ({
   gridContainerStyle: {
@@ -35,15 +36,16 @@ const useStyles = makeStyles((theme) => ({
       display: "flex",
       position: "fixed",
       top: 77,
-      backgroundColor:"#fafafa",
-      height:10,
+      backgroundColor: "#fafafa",
+      height: 10,
       color: theme.palette.primary.light,
-      borderStyle:"solid",
-      borderRadius:"0.5rem",
+      borderStyle: "solid",
+      borderRadius: "0.5rem",
       zIndex: 9,
     },
   },
 }));
+
 
 export default function ShoppingCart() {
   const { cart, totalCartPrice } = useContext(CartContext);
@@ -56,40 +58,59 @@ export default function ShoppingCart() {
 
   return (
     <Container>
-      <Grid container className={classes.gridContainerStyle}>
-        <Grid item xs={12} className={classes.topGridFlex}>
-          <Box className={classNames(classes.stickyContent,classes.topGridFlex)}>
-            <Box className={classes.topGridFlex}>
-              <Typography variant="h5">Shopping</Typography>
+      {cart.length === 0 ? (
+        <Box className={classes.topGridFlex}>
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <Typography variant="h4">Cart is empty</Typography>
+            <Box>
+              <SentimentVeryDissatisfiedIcon />
               <ShoppingCartOutlinedIcon />
-              <Typography variant="h5" style={{ color: "green" }}>
-                ({totalItems})
-              </Typography>
-            </Box>
-            <Box className={classes.topGridFlex}>
-              <Typography variant="h5"> {totalCartPrice()}</Typography>
-              <EuroSymbolOutlinedIcon style={{ color: "green" }} />
             </Box>
           </Box>
-        </Grid>
-        <Grid container direction="column">
-          {cart.map((product) => (
-            <SlipCard key={product.id} product={product} />
-          ))}
-        </Grid>
-      </Grid>
-      <Grid item className={classes.bottomGridFlex}>
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={() => setShowForm(true)}
-        >
-          CheckOut
-        </Button>
-        <Container className={classes.formStyle}>
-          {showForm && <CheckoutForm />}
+        </Box>
+      ) : (
+        <Container>
+          <Grid container className={classes.gridContainerStyle}>
+            <Grid item xs={12} className={classes.topGridFlex}>
+              <Box
+                className={classNames(
+                  classes.stickyContent,
+                  classes.topGridFlex
+                )}
+              >
+                <Box className={classes.topGridFlex}>
+                  <Typography variant="h5">Shopping</Typography>
+                  <ShoppingCartOutlinedIcon />
+                  <Typography variant="h5" style={{ color: "green" }}>
+                    ({totalItems})
+                  </Typography>
+                </Box>
+                <Box className={classes.topGridFlex}>
+                  <Typography variant="h5"> {totalCartPrice()}</Typography>
+                  <EuroSymbolOutlinedIcon style={{ color: "green" }} />
+                </Box>
+              </Box>
+            </Grid>
+            <Grid container direction="column">
+              {cart.map((product) => (
+                <SlipCard key={product.id} product={product} />
+              ))}
+            </Grid>
+          </Grid>
+          <Grid item className={classes.bottomGridFlex}>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => setShowForm(true)}
+            >
+              CheckOut
+            </Button>
+            <Container className={classes.formStyle}>
+              {showForm && <CheckoutForm />}
+            </Container>
+          </Grid>
         </Container>
-      </Grid>
+      )}
     </Container>
   );
 }
