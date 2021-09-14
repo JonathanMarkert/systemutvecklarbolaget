78f3f34c8005e88e5d8b9924166633ad72f2ad1f
Alpha-85 <ljungdahlt@gmail.com>
2021-09-14 19:56:27 +0200

    added yup object

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -3,28 +3,47 @@ import { Product } from "../Interfaces/IProduct";
 import { Button, DialogActions, TextField } from "@material-ui/core";
 import { ProductContext } from "./context/ProductContext";
 import * as yup from "yup";
-import {useFormik} from "formik";
+import { useFormik } from "formik";
 
 interface Props {
-  product?: Product,
+  product?: Product;
   // handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
   handleClose: () => void;
 }
 
 let defaultProduct: Product = {
-  id: '',
-  name:  '',
-  brewery: '',
-  url: '',
-  urlDetails: '',
-  description: '',
+  id: "",
+  name: "",
+  brewery: "",
+  url: "",
+  urlDetails: "",
+  description: "",
   price: 0,
   amount: 0,
-} 
+};
 
+const validationSchema = yup.object({
+  name: yup
+    .string()
+    .required("Beer must have a name")
+    .min(3, "Must be atleast 3 characters long")
+    .max(10, "Name is to long"),
+  brewery: yup
+    .string()
+    .required("Brewery must have a name")
+    .min(3, "Must be atleast 3 characters long")
+    .max(10, "Name is to long"),
+  url: yup.string().notRequired(),
+  urlDetails: yup.string().notRequired(),
+  description: yup.string().notRequired().max(30, "Description is to long"),
+  price: yup
+    .number()
+    .required("Beer aint free!")
+    .min(1, "minimum value of one"),
+});
 
-export default function ProductForm ({ product, handleClose }: Props) {
-  const [newProduct, setNewProduct] = useState<Product>(defaultProduct); 
+export default function ProductForm({ product, handleClose }: Props) {
+  const [newProduct, setNewProduct] = useState<Product>(defaultProduct);
   const { addBeerProduct, editBeerProduct } = useContext(ProductContext);
 
   const formik = useFormik({
@@ -39,10 +58,10 @@ export default function ProductForm ({ product, handleClose }: Props) {
       amount: 0,
     },
     onSubmit: (values) => {
-      console.log(JSON.stringify(values))
-    }
+      console.log(JSON.stringify(values));
+    },
   });
-  
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (!product) {
       const { value, name, type } = event.target;
@@ -53,100 +72,99 @@ export default function ProductForm ({ product, handleClose }: Props) {
       const inputValue = type === "number" ? Number(value) : value;
       setNewProduct({ ...product, [name]: inputValue });
     }
-  }
+  };
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      if (!newProduct) return;
-      if (!product) {
-        addBeerProduct(newProduct);
-      }
-      else {
-        editBeerProduct(newProduct);
-      }
-      handleClose();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!newProduct) return;
+    if (!product) {
+      addBeerProduct(newProduct);
+    } else {
+      editBeerProduct(newProduct);
     }
-      
-    return (
-      <form noValidate autoComplete="off" onSubmit={formik.handleSubmit}>
-        <TextField
-          name="name"
-          type="text"
-          onChange={formik.handleChange}
-          // onChange={(e) => setProduct({ ...product, price: Number(e.target.value) })}
-          margin="dense"
-          label="Name"
-          defaultValue={product?.name}
-          variant="filled"
-          fullWidth
-          value={formik.values.name}
-        />
-        <TextField
-          name="brewery"
-          type="text"
-          onChange={formik.handleChange}
-          margin="dense"
-          label="Brewery"
-          defaultValue={product?.brewery}
-          variant="filled"
-          fullWidth
-          value={formik.values.brewery}
-        />
-        <TextField
-          name="url"
-          type="text"
-          onChange={formik.handleChange}
-          margin="dense"
-          label="Image URL"
-          defaultValue={product?.url}
-          variant="filled"
-          fullWidth
-          value={formik.values.url}
-        />
-        <TextField
-          name="urlDetails"
-          type="text"
-          onChange={formik.handleChange}
-          margin="dense"
-          label="Alternative image URL"
-          defaultValue={product?.urlDetails}
-          variant="filled"
-          fullWidth
-          value={formik.values.urlDetails}
-        />
-        <TextField
-          name="description"
-          type="text"
-          onChange={formik.handleChange}
-          margin="dense"
-          label="Description"
-          defaultValue={product?.description}
-          variant="filled"
-          fullWidth
-          multiline
-          rows={4}
-          value={formik.values.description}
-        />
-        <TextField
-          name="price"
-          type="number"
-          onChange={formik.handleChange}
-          margin="dense"
-          label="Price"
-          defaultValue={product?.price}
-          variant="filled"
-          fullWidth
-          value={formik.values.price}
-        />
+    handleClose();
+  };
 
-        <DialogActions>
-          <Button type="submit" color="secondary">
-            Save
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-        </DialogActions>
-      </form>
-    );
+  return (
+    <form noValidate autoComplete="off" onSubmit={formik.handleSubmit}>
+      <TextField
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        // onChange={(e) => setProduct({ ...product, price: Number(e.target.value) })}
+        margin="dense"
+        label="Name"
+        defaultValue={product?.name}
+        variant="filled"
+        fullWidth
+        value={formik.values.name}
+      />
+      <TextField
+        name="brewery"
+        type="text"
+        onChange={formik.handleChange}
+        margin="dense"
+        label="Brewery"
+        defaultValue={product?.brewery}
+        variant="filled"
+        fullWidth
+        value={formik.values.brewery}
+      />
+      <TextField
+        name="url"
+        type="text"
+        onChange={formik.handleChange}
+        margin="dense"
+        label="Image URL"
+        defaultValue={product?.url}
+        variant="filled"
+        fullWidth
+        value={formik.values.url}
+      />
+      <TextField
+        name="urlDetails"
+        type="text"
+        onChange={formik.handleChange}
+        margin="dense"
+        label="Alternative image URL"
+        defaultValue={product?.urlDetails}
+        variant="filled"
+        fullWidth
+        value={formik.values.urlDetails}
+      />
+      <TextField
+        name="description"
+        type="text"
+        onChange={formik.handleChange}
+        margin="dense"
+        label="Description"
+        defaultValue={product?.description}
+        variant="filled"
+        fullWidth
+        multiline
+        rows={4}
+        value={formik.values.description}
+      />
+      <TextField
+        name="price"
+        type="number"
+        onChange={formik.handleChange}
+        margin="dense"
+        label="Price"
+        defaultValue={product?.price}
+        variant="filled"
+        fullWidth
+        value={formik.values.price}
+      />
+
+      <DialogActions>
+        <Button type="submit" color="secondary">
+          Save
+        </Button>
+        <Button onClick={handleClose} color="primary">
+          Cancel
+        </Button>
+      </DialogActions>
+    </form>
+  );
 }
